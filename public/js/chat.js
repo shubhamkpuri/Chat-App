3cfd059761d89c27eb162743aa12c51c10343752
Shubham Kumar Puri <shubhampuri8169@gmail.com>
2018-07-15 13:17:06 +0530

    Added join page

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom(){
+    //Selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+    // height
+    var clientHeight= messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
+
 socket.on('connect',function (){
     console.log("connected to server");
     // socket.emit('createMessage',{
@@ -21,7 +38,7 @@ socket.on('newMessage', function (message){
         createdAt:formatedTime
     });
     $('#messages').append(html);
-
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage',function (message){
@@ -33,6 +50,7 @@ socket.on('newLocationMessage',function (message){
         createdAt:formatedTime
     });
     $('#messages').append(html);
+    scrollToBottom();
 
 })
 
